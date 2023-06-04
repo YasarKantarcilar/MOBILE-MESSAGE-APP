641fa8433c3cd0a85f6f86073f5a0ea214752725
Yaşar KANTARCILAR <108579650+YasarKantarcilar@users.noreply.github.com>
2023-06-04 23:31:21 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMCgBJBFWytEi_2-O3nnugynZmTeyeCeg",
-  authDomain: "messageapp-2337e.firebaseapp.com",
-  projectId: "messageapp-2337e",
-  storageBucket: "messageapp-2337e.appspot.com",
-  messagingSenderId: "179248709272",
-  appId: "1:179248709272:web:58f6bfebff4445ad991aed",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
